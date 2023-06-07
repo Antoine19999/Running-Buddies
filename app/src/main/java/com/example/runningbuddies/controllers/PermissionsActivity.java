89320f81883ce07979070a77c9095ded9820b620
Antoine SEDOH <anthonysedoh@gmail.com>
2023-06-07 17:26:26 +0200

    Permission page

---
+++ app/src/main/java/com/example/runningbuddies/controllers/PermissionsActivity.java
@@ -0,0 +1,16 @@
+package com.example.runningbuddies.controllers;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.runningbuddies.R;
+
+public class PermissionsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_permissions);
+    }
+}
