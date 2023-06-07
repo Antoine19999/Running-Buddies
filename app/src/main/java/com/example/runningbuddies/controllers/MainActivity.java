5cbdb7debaf7da07b0879bdba5d5ac122306b71b
Akouete Antonin Sedoh <antonin.sedoh@etu.unige.ch>
2023-06-08 00:06:47 +0200

    Request location permissions ok

--- app/src/main/java/com/example/runningbuddies/controllers/MainActivity.java
+++ app/src/main/java/com/example/runningbuddies/controllers/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.runningbuddies.controllers;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import com.example.runningbuddies.R;
 import com.google.firebase.auth.FirebaseAuth;
@@ -15,9 +18,11 @@ public class MainActivity extends AppCompatActivity {
 
 
     FirebaseAuth auth;
-    Button logoutButton, mapButton;
+    Button logoutButton, mapButton, searchButton;
     TextView textView;
     FirebaseUser user;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
         auth = FirebaseAuth.getInstance();
         logoutButton = findViewById(R.id.logout);
         mapButton = findViewById(R.id.map);
+        searchButton = findViewById(R.id.settings);
         user = auth.getCurrentUser();
 
         if (user == null){
@@ -51,5 +57,27 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(getApplicationContext(), MapsActivity.class);
             startActivity(intent);
         });
+
+        searchButton.setOnClickListener(view -> {
+            Intent intent = new Intent(getApplicationContext(), SearchActivity.class);
+            startActivity(intent);
+        });
+
+        requestForPermissions();
     }
+
+    /**
+     * Check for permissions and request if necessary
+     */
+    private void requestForPermissions() {
+        // Check if permissions are granted. If not then ask
+        if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACTIVITY_RECOGNITION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+            }, 100);
+        }
+    }
+
 }
