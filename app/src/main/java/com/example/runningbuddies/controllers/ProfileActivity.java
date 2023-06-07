76eb6f5a30a4a1b575c60b211caa073acf59ea2c
Akouete Antonin Sedoh <antonin.sedoh@etu.unige.ch>
2023-06-07 22:56:05 +0200

    Save to db ok

--- app/src/main/java/com/example/runningbuddies/controllers/ProfileActivity.java
+++ app/src/main/java/com/example/runningbuddies/controllers/ProfileActivity.java
@@ -1,15 +1,7 @@
 package com.example.runningbuddies.controllers;
-import com.example.runningbuddies.interfaces.User;
-
-
-
-import androidx.appcompat.app.AppCompatActivity;
-
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,7 +9,10 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.runningbuddies.R;
+import com.example.runningbuddies.interfaces.User;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
@@ -61,14 +56,14 @@ public class ProfileActivity extends AppCompatActivity {
         Spinner spinner = findViewById(R.id.spinner);
 
         //First spinner
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this,
                 android.R.layout.simple_spinner_item, arrayList);
         arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(arrayAdapter);
 
         //Second spinner
         Spinner spinner_max = findViewById(R.id.spinner2);
-        ArrayAdapter<String> arrayAdapter1 = new ArrayAdapter<String>(this,
+        ArrayAdapter<String> arrayAdapter1 = new ArrayAdapter<>(this,
                 android.R.layout.simple_spinner_item, arrayList);
         arrayAdapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner_max.setAdapter(arrayAdapter1);
@@ -84,60 +79,49 @@ public class ProfileActivity extends AppCompatActivity {
             radioGroupGender = findViewById(R.id.radio_group_gender);
             radioGroupRun = findViewById(R.id.radio_group_run);
 
-            submitUserInformation.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    String firstName, lastName, userName;
-                    int age;
-                    firstName = String.valueOf(editTextFirstName.getText());
-                    lastName = String.valueOf(editTextLastName.getText());
-                    userName = String.valueOf(editTextUserName.getText());
-                    age = Integer.parseInt(editTextAge.getText().toString());
+            submitUserInformation.setOnClickListener(view -> {
+                String firstName, lastName, userName;
+                int age;
+                firstName = String.valueOf(editTextFirstName.getText());
+                lastName = String.valueOf(editTextLastName.getText());
+                userName = String.valueOf(editTextUserName.getText());
+                age = Integer.parseInt(editTextAge.getText().toString());
 
-                    // get selected radio button from radioGroup
-                    int genderSelectedId = radioGroupGender.getCheckedRadioButtonId();
-                    int runSelectedId = radioGroupRun.getCheckedRadioButtonId();
+                // get selected radio button from radioGroup
+                int genderSelectedId = radioGroupGender.getCheckedRadioButtonId();
+                int runSelectedId = radioGroupRun.getCheckedRadioButtonId();
 
-                    // find the radiobutton by returned id
-                    selectedRadioButtonGender = findViewById(genderSelectedId);
-                    selectedRadioButtonRun = findViewById(runSelectedId);
+                // find the radiobutton by returned id
+                selectedRadioButtonGender = findViewById(genderSelectedId);
+                selectedRadioButtonRun = findViewById(runSelectedId);
 
-                    Spinner minSpinner = findViewById(R.id.spinner);
-                    Spinner maxSpinner = findViewById(R.id.spinner2);
-                    String minSpeed = minSpinner.getSelectedItem().toString();
-                    String maxSpeed = maxSpinner.getSelectedItem().toString();
+                Spinner minSpinner = findViewById(R.id.spinner);
+                Spinner maxSpinner = findViewById(R.id.spinner2);
+                String minSpeed = minSpinner.getSelectedItem().toString();
+                String maxSpeed = maxSpinner.getSelectedItem().toString();
 
-                    // Get current user
-                    FirebaseUser currentUser = mAuth.getCurrentUser();
-                    if (currentUser != null) {
-                        String userUid = currentUser.getUid();
-                        String userEmail = currentUser.getEmail();
-                        // Write a message to the database
-                        mDatabase = FirebaseDatabase.getInstance().getReference();
+                // Get current user
+                FirebaseUser currentUser = mAuth.getCurrentUser();
+                if (currentUser != null) {
+                    String userUid = currentUser.getUid();
+                    String userEmail = currentUser.getEmail();
+                    // Write a message to the database
+                    mDatabase = FirebaseDatabase.getInstance().getReference();
 
-                        // Create a new User
-                        User user = new User(userName, userEmail, firstName, lastName, age,
-                                selectedRadioButtonGender.toString(),
-                                selectedRadioButtonRun.toString(), minSpeed, maxSpeed);
+                    // Create a new User
+                    User user = new User(userName, userEmail, firstName, lastName, age,
+                            selectedRadioButtonGender.getText().toString(),
+                            selectedRadioButtonRun.getText().toString(), minSpeed, maxSpeed);
 
-                        // Save user info
-                        mDatabase.child("users").child(userUid).setValue(user);
+                    // Save user info
+                    mDatabase.child("users").child(userUid).setValue(user);
 
-                        // Go to main activity
-                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                        startActivity(intent);
-                        finish();
-                    }
-
-                    Log.d("Information", "Gender = " + selectedRadioButtonGender.getText()
-                            + "Speed = " + selectedRadioButtonRun.getText()
-                            + "firstName = " + firstName
-                            + "lastName = " + lastName
-                            + "userName = " + userName
-                            + "age = " + age
-                            + "minSpeed = " + minSpeed
-                            + "maxSpeed = " + maxSpeed);
+                    // Go to main activity
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent);
+                    finish();
                 }
+
             });
         }
 
